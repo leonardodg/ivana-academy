fb9bba655e7151efb95f0e98c53252f5046941a4
LeoDG <leonardodg@msn.com>
2025-01-24 15:02:09 -0300

    File config-docker.php

---
+++ config-docker.php
@@ -0,0 +1,27 @@
+<?php
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = getenv('MOODLE_DBTYPE');
+$CFG->dblibrary = getenv('MOODLE_DBLIB');
+$CFG->dbhost    = getenv('MOODLE_DBHOST');
+$CFG->dbname    = getenv('MOODLE_DBNAME');
+$CFG->dbuser    = getenv('MOODLE_DBUSER');
+$CFG->dbpass    = getenv('MOODLE_DBPASS');
+$CFG->prefix    = getenv('MOODLE_DBPFX');
+$CFG->dboptions = array(
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => getenv('MOODLE_DBCOLL'),
+);
+
+$CFG->wwwroot   = getenv('MOODLE_URL');
+$CFG->dataroot  = getenv('MOODLE_DATA');
+$CFG->admin     = getenv('MOODLE_ADMIN');
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
